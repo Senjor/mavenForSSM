ba6631b9ba1b9a653155f9198d3a8a8f795f7773
Senjor <377844325@qq.com>
2018-11-21 19:25:18 +0800

    change some code for warning

--- src/main/java/com/zhiyou/controller/UserController.java
+++ src/main/java/com/zhiyou/controller/UserController.java
@@ -3,8 +3,6 @@ package com.zhiyou.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/zhiyou/service/impl/UserServiceImpl.java
+++ src/main/java/com/zhiyou/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.zhiyou.service.impl;
 
-import javax.annotation.Resource;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Service;
 
 import com.zhiyou.bean.User;
